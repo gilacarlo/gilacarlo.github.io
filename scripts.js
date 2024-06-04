6e25502afd831b44d5899733b0f0b8cf1413cdc2
gilacarlo <info.giancarlo2022.02@gmail.com>
2024-06-04 14:04:43 -0300

    opa

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+function proxImagem( img ){
+fetch(`img/${img}.jpg`)
+    .then(resp => resp.blob())
+    .then(blob => {
+        const imageObjectURL = URL.createObjectURL(blob);
+        console.log(imageObjectURL);
+        const proxImgTag = document.createElement("img");
+        proxImgTag.src = imageObjectURL;
+        document.getElementById("placeholder").appendChild(proxImgTag);
+    })
+}
