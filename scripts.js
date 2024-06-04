a2e3c9cd27527024a0a913628bc2fed35c271812
gilacarlo <info.giancarlo2022.02@gmail.com>
2024-06-04 14:16:16 -0300

    nao vai

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-
+let item = 0;
+const max = 80;
 
 function proxImagem( img ){
-fetch(`img/${img}.jpg`)
+fetch(`img/${img}.png`)
     .then(resp => resp.blob())
     .then(blob => {
         const imageObjectURL = URL.createObjectURL(blob);
@@ -10,4 +11,18 @@ fetch(`img/${img}.jpg`)
         proxImgTag.src = imageObjectURL;
         document.getElementById("placeholder").appendChild(proxImgTag);
     })
+}
+
+window.onscroll = function(){
+    let altura = document.body.scrollHeight;
+    let scrollPoint = window.scrollY + window.innerHeight;
+    if(scrollPoint >= altura){
+        proxImagem(item++ %max)
+    }
+}
+
+window.onload = function(){
+    for(item = 0; item <5; item++){
+        proxImagem(item);
+    }
 }
