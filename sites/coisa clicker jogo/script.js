8204582cb8a159ff18a114f0dbb88c7e5ddaaa8f
gilacarlo <info.giancarlo2022.02@gmail.com>
2024-07-08 13:04:21 -0300

    ocoisas

--- sites/coisa clicker jogo/script.js
+++ sites/coisa clicker jogo/script.js
@@ -1,4 +1,4 @@
-let coisas_$ = 30000;
+let coisas_$ = 0;
 let clickador_de_coisas_passive_income = 0; let BUFF_clickador_de_coisas_passive_income = 1;
 let lambedor_de_coisas_passive_income = 0; let BUFF_lambedor_de_coisas_passive_income = 1;
 let coisa_trabalhista_passive_income = 0; let BUFF_coisa_trabalhista_passive_income = 1;
